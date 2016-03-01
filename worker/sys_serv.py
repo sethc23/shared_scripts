28f3ae4c77d382d09f6a77027bafbcc584fcc076
mbp2 <blinddiver@gmail.com>
2016-02-29 22:12:25 -0500

    cleaned up repo and synced with existing branch before removing old branch

--- worker/sys_serv.py
+++ worker/sys_serv.py
@@ -2,12 +2,12 @@
 
 from app_factory                            import *
 
-sync_app, async_app                         =   celery_apps_factory(
+sys_serv, async_app                         =   celery_apps_factory(
                                                 app_type                    =   'SUBSCRIBER',
                                                 sync_broker_url             =   BROKER_URL,
                                                 async_broker_url            =   None,
                                                 backend                     =   'redis',
-                                                service_name                =   'sys_scr_app',
+                                                service_name                =   'sys_serv',
                                                 )
 
-from sys_scr_tasks                          import *
+from sys_serv_tasks                          import *
