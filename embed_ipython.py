8e61f8308251f0c3dea170bc4fe0c8f5aa590652
sethc23 <sethc23@users.noreply.github.com>
2016-03-14 14:39:19 -0400

    minor updates

--- embed_ipython.py
+++ embed_ipython.py
@@ -1,7 +1,7 @@
 
-from sys import path as py_path
-py_path.append('/home/ub2/SERVER2/ipython/ipython')
-from IPython import embed_kernel as embed
+# from sys import path as py_path
+# py_path.append('/home/ub2/SERVER2/ipython/ipython')
+# from IPython import embed_kernel as embed
 ## not working -- from ipdb import set_trace as i_trace
 
 # Usage:
@@ -15,3 +15,35 @@ from IPython import embed_kernel as embed
 
 #import embed_ipython as I; I.start_ipython(argv=["qtconsole","--profile=nbserver"]);
 
+
+from __future__ import print_function
+import os
+
+from IPython.kernel.inprocess import InProcessKernelManager
+from IPython.terminal.console.interactiveshell import ZMQTerminalInteractiveShell
+
+
+def print_process_id():
+    print('Process ID is:', os.getpid())
+
+
+def main():
+    print_process_id()
+
+    # Create an in-process kernel
+    # >>> print_process_id()
+    # will print the same process ID as the main process
+    kernel_manager = InProcessKernelManager()
+    kernel_manager.start_kernel()
+    kernel = kernel_manager.kernel
+    kernel.gui = 'qt4'
+    kernel.shell.push({'foo': 43, 'print_process_id': print_process_id})
+    client = kernel_manager.client()
+    client.start_channels()
+
+    shell = ZMQTerminalInteractiveShell(manager=kernel_manager, client=client)
+    shell.mainloop()
+
+main()
+if __name__ == '__main__':
+    main()
